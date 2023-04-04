cb18440df2208ecb66c1eae486f0455b01c0affe
muhammed2378 <salnhmohammed123@gmail.com>
2023-04-04 22:37:08 +0300

    added lorem text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,12 @@ const Navbar = () => {
             <div className='links'>
                 <a href='/'>Home</a>
                 <a href='/create'>New Blog</a>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quas tempora totam itaque nobis id minus porro! Alias
+                    voluptates voluptatem, facilis explicabo, ipsa assumenda,
+                    rerum dicta qui magni exercitationem voluptate! Molestias!
+                </p>
             </div>
         </div>
     );
